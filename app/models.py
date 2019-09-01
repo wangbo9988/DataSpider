a4aa5928e50a52b2f8e1ed5041dc88eb71798881
wangbo9988 <465204107@qq.com>
2019-09-01 10:35:42 +0800

    第四次提交

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+class data_spider(models.Model):
+    name = models.CharField(max_length=255, verbose_name='名称')
+    address = models.CharField(max_length=255, verbose_name='网址')
+    content = models.TextField(verbose_name='内容')
+
+    # 该字段的别名
+    class Meta():
+        verbose_name_plural = '爬虫'
+
+    def __str__(self):
+        return self.title
--- spider/spider/main.py
+++
@@ -1,7 +0,0 @@
-from scrapy import cmdline
-from spider.test import urls, X_Path
-
-urls.URLS.append('https://movie.douban.com/top250')
-X_Path.path = '//*[@id="content"]/div/div/ol/li/div/div/div/a/span[1]' + '/text()'
-
-cmdline.execute('scrapy crawl myspider'.split())
